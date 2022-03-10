47915acbf976c8f2bb8039e4512580aba0df70c3
UltraWyatt <100807663+UltraWyatt@users.noreply.github.com>
2022-03-10 15:39:16 -0600

    Add This Console File
    This Is very useful you can try it yourself if you don't trust it

---
+++ Dev-Console.js
@@ -0,0 +1,3 @@
+// This Will Open A console to paste in java script easily with a simple removal command
+// MUST be used as a bookmarklet
+javascript:var s=document.createElement('script');s.type='text/javascript';s.src='https://sheeptester.github.io/javascripts/eval.js';document.body.appendChild(s);void(0);
